3a2175c9d202f71c5d6e9aa672748b6949b5f91a
Gavo9649 <gprecurato@gmail.com>
2026-03-04 19:32:33 -0500

    Started committed to Feeder Code
    Feeder code added, to be continued

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -4,11 +4,26 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Feeder extends SubsystemBase {
+  private final SparkMax feedermotor = new SparkMax(30, MotorType.kBrushless);
+
+  private SparkMaxConfig feedermotorconfig = new SparkMaxConfig();
+
   /** Creates a new Feeder. */
-  public Feeder() {}
+  public Feeder() {
+    feedermotorconfig.idleMode(IdleMode.kBrake);
+
+    feedermotor.configure(
+        feedermotorconfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
 
   @Override
   public void periodic() {
