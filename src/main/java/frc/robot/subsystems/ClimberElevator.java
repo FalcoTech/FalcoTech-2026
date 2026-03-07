4b91f616483b27989d16d9a4c6d2cd94cb8910e3
Kevin Yacucci <kcy0511@gmail.com>
2026-03-07 11:16:58 -0500

    Update libs

--- src/main/java/frc/robot/subsystems/ClimberElevator.java
+++ src/main/java/frc/robot/subsystems/ClimberElevator.java
@@ -49,7 +49,9 @@ public class ClimberElevator extends SubsystemBase {
                   ClimbConstants.ELEVATOR_FF_kS,
                   ClimbConstants.ELEVATOR_FF_kG,
                   ClimbConstants.ELEVATOR_FF_kV))
-          .withSoftLimit(ClimbConstants.ELEVATOR_MIN_HEIGHT, ClimbConstants.ELEVATOR_MAX_HEIGHT.minus(Inches.of(0.125)))
+          .withSoftLimit(
+              ClimbConstants.ELEVATOR_MIN_HEIGHT,
+              ClimbConstants.ELEVATOR_MAX_HEIGHT.minus(Inches.of(0.125)))
           .withTelemetry("ClimberElevMotor", TelemetryVerbosity.HIGH)
           .withGearing(
               new MechanismGearing(
